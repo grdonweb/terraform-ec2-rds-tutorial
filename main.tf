eec5b9eaaf2efc9d6f7626619d9fbf158f89405c
Grdonweb <grdonweb@gmail.com>
2022-08-11 11:37:02 +0100

    steps done 5 to 6 security groups and db subnet group

--- main.tf
+++ main.tf
@@ -92,3 +92,68 @@ resource "aws_route_table_association" "private" {
   route_table_id = aws_route_table.tutorial_pri_rt.id
   subnet_id      = aws_subnet.tutorial_pri_subnet[count.index].id
 }
+
+//Step5.1: EC2 Security Groups
+
+resource "aws_security_group" "tutorial_web_sg" {
+
+  name        = "tutorial_web_sg"
+  description = "Security group for tutorial web servers"
+  vpc_id      = aws_vpc.tutorial_vpc.id
+
+  ingress {
+    description = "Allow all traffic through HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    description = "Allow SSH connections"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    description = "Allow all outbound traffic"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "tutorial_web_sg"
+  }
+}
+
+//Step5.2: RDS Security Groups
+
+resource "aws_security_group" "tutorial_db_sg" {
+
+  name        = "tutorial_db_sg"
+  description = "Security group for tutorial databases"
+  vpc_id      = aws_vpc.tutorial_vpc.id
+
+  ingress {
+    description     = "Allow Mysql traffic from only the web sg"
+    from_port       = 3306
+    to_port         = 3306
+    protocol        = "tcp"
+    security_groups = [aws_security_group.tutorial_web_sg.id]
+  }
+  tags = {
+    Name = "tutorial_db_sg"
+  }
+}
+
+//Step6: Creating db subnet group
+
+resource "aws_db_subnet_group" "tutorial_db_subnet_group" {
+  name        = "tutorial_db_subnet_group"
+  description = "DB subnet group for tutorial"
+
+  subnet_ids = [for subnet in aws_subnet.tutorial_pri_subnet : subnet.id]
+}
